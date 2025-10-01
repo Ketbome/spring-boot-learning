27f43e5513098bbcd0620cfc20ec5216a061c468
Ketbome <Pablo.moraga.san@gmail.com>
2025-10-01 14:25:52 -0300

    Add Docker Compose configuration for PostgreSQL and update POM with JPA and PostgreSQL dependencies; implement PlatziPlayAiService and HelloController for welcome message generation.

--- src/main/java/com/platzi/play/web/controller/HelloController.java
+++ src/main/java/com/platzi/play/web/controller/HelloController.java
@@ -1,8 +1,10 @@
-package com.platzi.play;
+package com.platzi.play.web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.platzi.play.domain.service.PlatziPlayAiService;
 
 @RestController
 public class HelloController {
