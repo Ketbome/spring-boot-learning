0568080ca7d372464253b4a1087828cf271bb43e
Ketbome <Pablo.moraga.san@gmail.com>
2025-10-01 14:02:26 -0300

    Update HelloController and PlatziPlayAiService to include platform name in welcome message; add application properties for development environment and adjust application name and context path.

--- src/main/java/com/platzi/play/HelloController.java
+++ src/main/java/com/platzi/play/HelloController.java
@@ -1,18 +1,21 @@
 package com.platzi.play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final String platform;
     private final PlatziPlayAiService platziPlayAiService;
 
-    public HelloController(PlatziPlayAiService platziPlayAiService) {
+    public HelloController(@Value("${spring.application.name}") String platform, PlatziPlayAiService platziPlayAiService) {
+        this.platform = platform;
         this.platziPlayAiService = platziPlayAiService;
     }
 
     @GetMapping("/")
     public String hello() {
-        return platziPlayAiService.generateWelcomeMessage();
+        return platziPlayAiService.generateWelcomeMessage(this.platform);
     }
 }
--- src/main/java/com/platzi/play/PlatziPlayAiService.java
+++ src/main/java/com/platzi/play/PlatziPlayAiService.java
@@ -1,13 +1,14 @@
 package com.platzi.play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAiService {
     
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de gestion de peliculas
+            Genera un saludo de bienvenida a la plataforma de gestion de peliculas {{platform}}
             """)
-    public String generateWelcomeMessage();
+    public String generateWelcomeMessage(@V("platform") String platform);
 }
